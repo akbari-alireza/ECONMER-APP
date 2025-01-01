fb022ed872d8e35b8785f9c12fc26ffc5fe829fd
akbari-alireza <alireza.akb1111@gmail.com>
2025-01-01 11:29:23 +0430

    Search Bar Part_2

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 const Collection = () => {
 
-  const { products } :any = useContext(ShopContext);
+  const { products, search, showSearch } :any = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -32,6 +32,10 @@ const Collection = () => {
 
   const applyFilter = ()=>{
     let productsCopy = products.slice();
+
+    if(showSearch && search) {
+      productsCopy = productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()))
+    }
 
     if(category.length > 0){
       productsCopy = productsCopy.filter(item=>category.includes(item.category))
@@ -70,7 +74,7 @@ useEffect(() => {
 
   useEffect(()=>{
     applyFilter();
-  },[category,subCategory])
+  },[category,subCategory, search, showSearch])
 
   useEffect(()=>{
 
