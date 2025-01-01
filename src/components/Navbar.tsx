388c202e3780ac7be7fd12da9fd1c6984c9fc7d6
akbari-alireza <alireza.akb1111@gmail.com>
2025-01-01 10:50:21 +0430

    Search Bar Part_1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {assets} from '../assets/frontend_assets/assets.ts'
 import { NavLink , Link} from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext.tsx'
 
 
 function Navbar() {
 
     const [visible,setVisible] = useState(false)
+    const {setShowSearch} = useContext(ShopContext)
 
 
   return (
@@ -36,7 +38,7 @@ function Navbar() {
         </ul>
         
         <div className='flex items-center gap-6'>
-            <img className='w-5 cursor-pointer' src={assets.search_icon} alt="" />
+            <img onClick={()=>setShowSearch(true)} className='w-5 cursor-pointer' src={assets.search_icon} alt="" />
             <div className='group relative'>
                 <img className='w-5 min-w-5 cursor-pointer' src={assets.profile_icon} alt=""  />
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
